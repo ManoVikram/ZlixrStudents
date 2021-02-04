9f64f4f59110087c18b52d9d30008db31e33f005
Mano Vikram <manovik18@gmail.com>
2021-02-04 16:06:28 +0530

    Added jsonwt strategy

---
+++ API/strategies/jsonwtStrategy.js
@@ -0,0 +1,29 @@
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
+const Student = mongoose.model("student");
+const myKey = require("../setup/urls");
+
+var opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = myKey.secret;
+
+module.exports = passport => {
+    passport.use(
+        new JwtStrategy(
+            opts,
+            (jwt_payload, done) => {
+                Student.findById(jwt_payload).then(
+                    (student) => {
+                        if (student) {
+                            return done(null, student);
+                        }
+                        return done(null, false);
+                    },
+                ).catch(
+                    (error) => console.log(error),
+                );
+            },
+        ),
+    );
+};
